05971872b816ac5770dbc5d3098cecd640d90610
sajadsj <sajadsojoudi@gmail.com>
2022-03-29 03:11:32 +0430

    categories route

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
-
+const categoryRoute = require("./routes/categories");
 
 app.use(express.json());
 mongoose.connect(
@@ -18,6 +18,7 @@ mongoose.connect(
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running...");
