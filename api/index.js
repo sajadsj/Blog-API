809024aa279a41aaf25211350394a5be232b5a89
sajadsj <sajadsojoudi@gmail.com>
2022-03-29 03:18:12 +0430

    upload a pic with multer

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
 
 app.use(express.json());
 mongoose.connect(
@@ -13,6 +15,25 @@ mongoose.connect(
 })
     .then(console.log("Connected to mongoDB!"))
     .catch((err) => console.log(err));
+
+
+
+//Upload Image
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
 
 
 app.use("/api/auth", authRoute);
